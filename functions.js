e8fbe0e9d9713072d676372de0b2d3ae71184dac
Edd-wordd <edward.plasencio@gmail.com>
2020-05-12 10:37:40 -0700

    add multiply function

--- functions.js
+++ functions.js
@@ -1,5 +1,13 @@
-// subract function
-function subract(input1, input2) {
+// add function
+function multiply(input1, input2) {
+   // A1: any JavaScript value
+   // A2: any JavaScript value
+   // R: a single JavaScript value
+   return input1 * input2;
+}
+
+// subtract function
+function subtract(input1, input2) {
    // Argument 1: any JavaScript value
    // Argument 2: any JavaScript value
    // Return: a single JavaScript value
