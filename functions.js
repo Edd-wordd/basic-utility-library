c7d11330260efda6c72019f385bf4ec4863c6cdd
Edd-wordd <edward.plasencio@gmail.com>
2020-05-12 09:34:54 -0700

    add subract function

--- functions.js
+++ functions.js
@@ -1,3 +1,11 @@
+// subract function
+function subract(input1, input2) {
+   // Argument 1: any JavaScript value
+   // Argument 2: any JavaScript value
+   // Return: a single JavaScript value
+   return input1 - input2;
+}
+// add function
 function add(input1, input2) {
    // A1: any JavaScript value
    // A2: any JavaScript value
